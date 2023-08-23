4fd1de0036bc368d27fa0b95c72af4e47efbad86
Konstantsin Shumak <konstantsin.shumak@gmail.com>
2023-08-23 11:59:07 +0200

    feat: change config

--- src/common/config.js
+++ src/common/config.js
@@ -6,16 +6,17 @@ dotenv.config({
 });
 
 module.exports = {
-  PORT: process.env.PORT,
+  PORT: 9999,
   NODE_ENV: process.env.NODE_ENV,
-  MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
+  MONGO_CONNECTION_STRING:
+    'mongodb+srv://react-english-learn:react-english-learn@cluster0.qjbpy0x.mongodb.net/',
   AUTH_MODE: process.env.AUTH_MODE === 'true',
   MAX_SYMBOLS_PER_OBJECT: 10000,
   MAX_OPTIONAL_PROPERTIES: 100,
   MIN_PASSWORD_LENGTH: 8,
   LOGS_DIR: path.join(__dirname, '../../logs'),
-  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+  JWT_SECRET_KEY: 'JWT_SECRET_KEY',
   JWT_EXPIRE_TIME: '4h',
-  JWT_REFRESH_SECRET_KEY: process.env.JWT_REFRESH_SECRET_KEY,
+  JWT_REFRESH_SECRET_KEY: 'JWT_REFRESH_SECRET_KEY',
   JWT_REFRESH_EXPIRE_TIME: 4.5 * 60 * 60
 };
